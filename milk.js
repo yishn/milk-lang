daab99bc13d63485fbece294ab5c8b635505212a
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-14 00:56:42 +0200

    First commit

---
+++ milk.js
@@ -0,0 +1,13 @@
+var process = require('process')
+var fs = require('fs')
+var nearley = require('nearley')
+var grammar = require('./grammar')
+
+var p = new nearley.Parser(grammar.ParserRules, grammar.ParserStart)
+var filename = process.argv[2]
+var data = fs.readFileSync(filename, 'utf8')
+var result = p.feed(data)
+
+console.dir(result.results[0], {
+    depth: null
+})
