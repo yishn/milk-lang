e503ac68e42471bfbb3e175409e9abd02c47664a
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-17 16:55:30 +0200

    Add underscore helper functions

---
+++ underscore.js
@@ -0,0 +1,26 @@
+var _ = {}
+
+_.modulo = function(a, b) {
+    var c = a % b;
+    return c >= 0 ? c : c + b;
+};
+
+_.enumerate = function(list) {
+    if (!Array.isArray(list) && typeof list !== 'string')
+        list = Object.keys(list);
+
+    return {
+        get: function(i) { return list[i] },
+        length: list.length
+    };
+};
+
+_.enumerateKeys = function(list) {
+    if (!Array.isArray(list) && typeof list !== 'string')
+        return this.enumerate(list);
+
+    return {
+        get: function(i) { return i },
+        length: list.length
+    };
+};
