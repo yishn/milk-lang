3f059d98ac99a7090093f8f001a378c5d8e3da32
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-18 15:40:30 +0200

    Add _.getIndex

--- underscore.js
+++ underscore.js
@@ -43,3 +43,10 @@ _.extends = function(child, parent) {
     child.__super__ = parent.prototype;
     return child;
 }
+
+_.getIndex = function(list, index) {
+    if (!Array.isArray(list) && typeof list !== 'string')
+        return list[index]
+
+    return list[_.modulo(index, list.length)]
+}
