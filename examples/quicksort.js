e35bb47c300f01a02e98bd22087fbe3df8051dc7
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-21 22:05:53 +0200

    Update quicksort example

--- examples/quicksort.js
+++ examples/quicksort.js
@@ -5,7 +5,7 @@ quicksort = function(list) {
     var pivot, rest, smaller, bigger;
     if (list.length === 0) {
         return [];
-    }
+    };
     /*@4:5*/
     (function(ref) {
         pivot = ref[0];
@@ -20,8 +20,17 @@ quicksort = function(list) {
         return x > pivot;
     }));
     /*@8:5*/
-    return smaller.concat([pivot]).concat(bigger);
-}
+    return (function() {
+        var r;
+        r = [];
+        r.push.apply(r, smaller);
+        r.push(pivot);
+        r.push.apply(r, bigger);
+        return r;
+    })();
+};
+/*@10:1*/
+console.log(quicksort([453, 45, 234, 46, 34, 4, 24, 56]));
 })();
 
-//: 1088ms
+//: 1280ms
