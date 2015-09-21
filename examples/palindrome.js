5d5c244a79fe45beb78885d81a9e6ca2c6e55bb2
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-21 21:25:54 +0200

    Add palindrome example

---
+++ examples/palindrome.js
@@ -0,0 +1,21 @@
+(function() {
+var _ = {}, palindrome;
+/*@1:1*/
+palindrome = function(input) {
+    var first, rest, last;
+    if (input.length === 0) {
+        return true;
+    };
+    /*@4:5*/
+    (function(ref) {
+        first = ref[0];
+        rest = (1 >= (ref.length - 1)) ? [] : [].slice.call(ref, 1, -1);
+        last = ref[ref.length - 1];
+        return ref;
+    })(input);
+    /*@5:5*/
+    return (first === last) && palindrome(rest);
+};
+})();
+
+//: 524ms
