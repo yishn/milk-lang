41381732d8d2ed8a1c6e7d839dc6d8b5524ee743
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-22 17:29:41 +0200

    Update primes generated code

--- examples/primes.js
+++ examples/primes.js
@@ -18,7 +18,7 @@ _.enumerate = function(l) {
  * @return array          the list of all primes below limit
  */
 getPrimes = function(limit) {
-    var checklist, l, i, checked;
+    var checklist, i1, l, checked, i;
 
     // Initialize checklist
     checklist = (function() {
@@ -29,14 +29,16 @@ getPrimes = function(limit) {
         step = (end === start) ? 1 : Math.sign(end - start);
         for (i = start; step > 0 ? i <= end : i >= end; i += step) {
             r.push(false);
-        }
+        };
         return r;
     })();
     /*@10:5*/
     checklist[0] = checklist[1] = true;
     l = checklist;
-    for (i in l) {
-        checked = l[i];
+    for (i1 in l) {
+        checked = l[i1];
+        i = parseInt(i1, 10);
+        if (isNaN(i)) i = i1;
         if (!(!checked)) continue;
         var start1, end1, step1, j;
         /*@14:9*/
@@ -46,22 +48,24 @@ getPrimes = function(limit) {
         step1 = ((i + 1) * i) - start1;
         for (j = start1; step1 > 0 ? j <= end1 : j >= end1; j += step1) {
             checklist[j] = true;
-        }
-    }
+        };
+    };
     /*@18:5*/
     // Accumulate unchecked items
     return (function() {
-        var r1, l1;
+        var r1, i2, l1;
         r1 = [];
         l1 = checklist;
-        for (i in l1) {
-            checked = l1[i];
+        for (i2 in l1) {
+            checked = l1[i2];
+            i = parseInt(i2, 10);
+            if (isNaN(i)) i = i2;
             if (!(!checked)) continue;
             r1.push(i);
-        }
+        };
         return r1;
     })();
-}
+};
 /*@28:1*/
 /**
  * Factorizes a given number in its unique prime
@@ -72,18 +76,18 @@ getPrimes = function(limit) {
  *                         of number
  */
 factorization = function(number) {
-    var primes, i1, l2, p;
+    var primes, i3, l2, p;
     if (number <= 1) {
         return [];
-    }
+    };
     /*@32:5*/
     // Get primes
     primes = getPrimes(number);
 
     // Search for divisor
     l2 = _.enumerate(primes);
-    for (i1 = 0; i1 < l2.length; i1++) {
-        p = l2[i1];
+    for (i3 = 0; i3 < l2.length; i3++) {
+        p = l2[i3];
         if (!(_.modulo(number, p) === 0)) continue;
         var result;
         /*@37:9*/
@@ -91,11 +95,11 @@ factorization = function(number) {
         result = factorization(number / p);
         result.push(p);
         return result;
-    }
-}
+    };
+};
 /*@41:1*/
 console.log('Here is your prime factorization for 8733:');
 console.log(factorization(8733));
 })();
 
-//: 9882ms
+//: 9263ms
