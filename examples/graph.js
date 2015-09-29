fe8f2252f4f97be3ad6c2b8c43ecf67ccf729525
Yichuan Shen <shen.yichuan@gmail.com>
2015-09-29 16:47:31 +0200

    Update graph example to include inheritance

--- examples/graph.js
+++ examples/graph.js
@@ -1,5 +1,5 @@
 (function() {
-var Graph, space, graph;
+var Graph, Labyrinth, labyrinth;
 enumerate = function(l) {
     var t = toString.call(l);
     if (t !== "[object Array]" && t !== "[object String]")
@@ -12,36 +12,41 @@ inOp = function(x, l) {
         return x in l;
     return l.indexOf(x) != -1;
 }
+extend = function(x, y) {
+    var copy = function() {}
+    copy.prototype = y.prototype;
+    var c = new copy();
+    c.constructor = x;
+    x.prototype = c;
+    x.prototype.__super__ = y.prototype;
+    x.prototype.__super__.init = y.prototype.constructor;
+    return x;
+}
 /*@1:1*/
 Graph = (function() {
     var init;
-    init = function() {
-        var vertices, edges, self, i3, l1, v1, v2;
-        (function(ref) {
-            vertices = ref[0];
-            edges = (1 >= ref.length) ? [] : [].slice.call(ref, 1);
-            return ref;
-        })(arguments);
+    init = function(vertices, edges) {
+        var self, i1, l1, v1, v2;
         self = this;
         /*@3:9*/
         self._adjacencyList = (function() {
-            var r, i2, l, v;
+            var r, i, l, v;
             r = {};
             l = enumerate(vertices);
-            for (i2 = 0; i2 < l.length; i2++) {
-                v = l[i2];
+            for (i = 0; i < l.length; i++) {
+                v = l[i];
                 r[v] = [];
             };
             return r;
         })();
         /*@5:9*/
         l1 = enumerate(edges);
-        for (i3 = 0; i3 < l1.length; i3++) {
-            (function(ref1) {
-                v1 = ref1[0];
-                v2 = ref1[1];
-                return ref1;
-            })(l1[i3]);
+        for (i1 = 0; i1 < l1.length; i1++) {
+            (function(ref) {
+                v1 = ref[0];
+                v2 = ref[1];
+                return ref;
+            })(l1[i1]);
             /*@6:13*/
             self.addEdge(v1, v2);
         };
@@ -57,11 +62,11 @@ Graph = (function() {
         self = this;
         /*@12:9*/
         return (function() {
-            var r1, i4, l2, v;
+            var r1, i2, l2, v;
             r1 = [];
             l2 = enumerate(self._adjacencyList);
-            for (i4 = 0; i4 < l2.length; i4++) {
-                v = l2[i4];
+            for (i2 = 0; i2 < l2.length; i2++) {
+                v = l2[i2];
                 r1.push(v);
             };
             return r1;
@@ -71,41 +76,51 @@ Graph = (function() {
         var self;
         self = this;
         /*@15:9*/
-        return self._adjacencyList[v];
+        return (function() {
+            var r2;
+            r2 = self._adjacencyList[v];
+            if (((typeof r2) === 'undefined') || (r2 == null)) {
+                return null;
+            };
+            return r2.slice;
+        })()(0);
     };
     init.prototype.addEdge = function(v1, v2) {
-        var self, i5, l3, x, y;
+        var self, i3, l3, x, y;
         self = this;
         /*@18:9*/
+        if (self.hasEdge(v1, v2)) {
+            return;
+        };
         l3 = enumerate(self._permute(v1, v2));
-        for (i5 = 0; i5 < l3.length; i5++) {
-            (function(ref2) {
-                x = ref2[0];
-                y = ref2[1];
-                return ref2;
-            })(l3[i5]);
-            /*@19:13*/
+        for (i3 = 0; i3 < l3.length; i3++) {
+            (function(ref1) {
+                x = ref1[0];
+                y = ref1[1];
+                return ref1;
+            })(l3[i3]);
+            /*@20:13*/
             self._adjacencyList[x].push(y);
         };
     };
     init.prototype.hasEdge = function(v1, v2) {
         var self;
         self = this;
-        /*@22:9*/
+        /*@23:9*/
         return inOp(v2, self._adjacencyList[v1]);
     };
     init.prototype.removeEdge = function(v1, v2) {
-        var self, i6, l4, x, y;
+        var self, i4, l4, x, y;
         self = this;
-        /*@25:9*/
+        /*@26:9*/
         l4 = enumerate(self._permute(v1, v2));
-        for (i6 = 0; i6 < l4.length; i6++) {
-            (function(ref3) {
-                x = ref3[0];
-                y = ref3[1];
-                return ref3;
-            })(l4[i6]);
-            /*@26:13*/
+        for (i4 = 0; i4 < l4.length; i4++) {
+            (function(ref2) {
+                x = ref2[0];
+                y = ref2[1];
+                return ref2;
+            })(l4[i4]);
+            /*@27:13*/
             self._adjacencyList[x] = self._adjacencyList[x].filter(function(x) {
                 return x !== y;
             });
@@ -114,7 +129,7 @@ Graph = (function() {
     init.prototype.addVertex = function(v) {
         var self;
         self = this;
-        /*@29:9*/
+        /*@30:9*/
         if (!inOp(v, self._adjacencyList)) {
             self._adjacencyList[v] = [];
         };
@@ -122,45 +137,89 @@ Graph = (function() {
     init.prototype.hasVertex = function(v) {
         var self;
         self = this;
-        /*@33:9*/
+        /*@34:9*/
         return inOp(v, self._adjacencyList);
     };
     init.prototype.removeVertex = function(v) {
-        var self, i7, l5, w;
+        var self, i5, l5, w;
         self = this;
-        /*@36:9*/
+        /*@37:9*/
         l5 = enumerate(self.getNeighbors(v));
-        for (i7 = 0; i7 < l5.length; i7++) {
-            w = l5[i7];
+        for (i5 = 0; i5 < l5.length; i5++) {
+            w = l5[i5];
             self.removeEdge(v, w);
         };
-        /*@39:9*/
+        /*@40:9*/
         delete self._adjacencyList[v];
     };
     return init;
 })();
-space = (function() {
-    var r2, start, end, step, i;
-    r2 = [];
-    start = 1;
-    end = 3;
-    step = (end === start) ? 1 : Math.sign(end - start);
-    for (i = start; step > 0 ? i <= end : i >= end; i += step) {
-        var start1, end1, step1, j;
-        start1 = 1;
-        end1 = 3;
-        step1 = (end1 === start1) ? 1 : Math.sign(end1 - start1);
-        for (j = start1; step1 > 0 ? j <= end1 : j >= end1; j += step1) {
-            r2.push((i + ',') + j);
-        };
+Labyrinth = (function() {
+    var init;
+    /*@43:5*/
+    init = function(width, height) {
+        var self, vertices, edges;
+        self = this;
+        vertices = (function() {
+            var r3, start, end, step, x;
+            r3 = [];
+            start = 1;
+            end = width;
+            step = (end === start) ? 1 : Math.sign(end - start);
+            for (x = start; step > 0 ? x <= end : x >= end; x += step) {
+                var start1, end1, step1, y;
+                start1 = 1;
+                end1 = height;
+                step1 = (end1 === start1) ? 1 : Math.sign(end1 - start1);
+                for (y = start1; step1 > 0 ? y <= end1 : y >= end1; y += step1) {
+                    r3.push([x, y]);
+                };
+            };
+            return r3;
+        })();
+        /*@45:9*/
+        edges = (function() {
+            var r4, i6, l6, v;
+            r4 = [];
+            l6 = enumerate(vertices);
+            for (i6 = 0; i6 < l6.length; i6++) {
+                v = l6[i6];
+                var i7, l7, w;
+                l7 = enumerate(vertices);
+                for (i7 = 0; i7 < l7.length; i7++) {
+                    w = l7[i7];
+                    if (!(self.distance(v, w) === 1)) continue;
+                    r4.push([v, w]);
+                };
+            };
+            return r4;
+        })();
+        /*@47:9*/
+        self.__super__.init.call(self, vertices, edges);
     };
-    return r2;
+    extend(init, Graph);
+    init.prototype.distance = function() {
+        var x1, y1, x2, y2, self;
+        (function(ref3) {
+            (function(ref4) {
+                x1 = ref4[0];
+                y1 = ref4[1];
+                return ref4;
+            })(ref3[0]);
+            (function(ref5) {
+                x2 = ref5[0];
+                y2 = ref5[1];
+                return ref5;
+            })(ref3[1]);
+            return ref3;
+        })(arguments);
+        self = this;
+        /*@50:9*/
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    };
+    return init;
 })();
-/*@42:1*/
-graph = new Graph(space, ['1,1', '1,2'], ['1,1', '1,3'], ['1,1', '2,1'], ['1,1', '3,1']);
-console.log(graph.getVertices());
-console.log(graph.getNeighbors('1,1'));
-/*@47:1*/
-graph.removeVertex('1,3');
-console.log(graph.getNeighbors('1,1'));
+labyrinth = new Labyrinth(10, 5);
+/*@53:1*/
+console.log(labyrinth.getNeighbors([3, 3]));
 })();
